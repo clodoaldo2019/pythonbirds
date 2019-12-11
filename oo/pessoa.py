33194364ac8ca7fb823e60df260fa8f1db74177e
Clodoaldo Corrêa <clodoaldo.scorrea@gmail.com>
2019-12-11 18:17:35 -0400

    Criada a classe Pessoa Criado método cumprimentar

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,8 +1,9 @@
-# class Pessoa:
-#     pass
+class Pessoa:
+    def cumprimentar(self):
+        return f'Olá {id(self)}'
 
-
-
-x = 100 % 2
-
-print(x)
+if __name__ == '__main__':
+    p = Pessoa()
+    print(Pessoa.cumprimentar(p))
+    print(id(p))
+    print(p.cumprimentar())
